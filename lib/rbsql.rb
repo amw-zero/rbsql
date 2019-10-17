d77cee2c4f7ab7dcf5afa3923a25a40352eb9485
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-10-17 18:43:25 -0400

    Initial commit

---
+++ lib/rbsql.rb
@@ -0,0 +1,6 @@
+require "rbsql/version"
+
+module Rbsql
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/rbsql_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class RbsqlTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Rbsql::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
